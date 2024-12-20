2251a3bbbabd879a471a5a286f2cfb8de5fb91ed
rafa <rescobar@bit.lat>
2024-12-20 14:41:41 -0600

    Agrega estructura de funciones de login y register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Get\AuthRequest;
+use App\Http\Requests\Store\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(AuthRequest $request)
     {
         try {
+            if (Auth::attempt($request->all())) {
+                $user = User::findOrFail(Auth::user()->id);
+                $token = $user->createToken('auth_token')->plainTextToken;
+            }
             
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $user = User::create($request->all());
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
