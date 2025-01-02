9022daa0d077e04b98d26cec9754bd36d14667c3
rafa <rescobar@bit.lat>
2025-01-02 13:32:29 -0600

    Libera API de Categorias

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PriorityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,5 @@ Route::controller(AuthController::class)->group(function(){
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::resource('/priority', PriorityController::class)->only(['index']);
+    Route::resource('/category', CategoryController::class)->only(['index']);
 });
