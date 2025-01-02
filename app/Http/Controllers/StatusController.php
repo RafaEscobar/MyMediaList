5b01ac12277f10ab7783486d110983a1f6968298
rafa <rescobar@bit.lat>
2025-01-02 13:36:52 -0600

    Libera API de Status

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Collections\StatusCollection;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class StatusController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $statuses = Status::all();
+            return new StatusCollection($statuses);
+        } catch (\Throwable $th) {
+            return response()->json(["message" => $th->getMessage()], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PriorityController;
+use App\Http\Controllers\StatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::resource('/priority', PriorityController::class)->only(['index']);
     Route::resource('/category', CategoryController::class)->only(['index']);
+    Route::resource('/status', StatusController::class)->only(['index']);
 });
