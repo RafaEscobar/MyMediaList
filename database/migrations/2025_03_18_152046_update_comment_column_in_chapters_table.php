39cf7ccf9c48d30cf69c6ab0a925803e2da99735
rafa <rescobar@bit.lat>
2025-03-18 09:22:56 -0600

    Ajuste

--- database/migrations/2025_03_18_152046_update_comment_column_in_chapters_table.php
+++ database/migrations/2025_03_18_152046_update_comment_column_in_chapters_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('chapters', function (Blueprint $table) {
-            $table->text('comment')->nullable();
+            $table->text('comment')->nullable()->change();
         });
     }
 
