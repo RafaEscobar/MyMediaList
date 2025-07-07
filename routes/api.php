1cafec5dc612c50dc23d283df775242dcde78a5b
rafa <Rafael.EG.code@gmail.com>
2025-07-07 13:40:10 -0600

    Limpiando proyecto

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ChapterController;
-use App\Http\Controllers\FavoriteController;
-use App\Http\Controllers\MediumController;
-use App\Http\Controllers\PendingPriorityController;
-use App\Http\Controllers\PostViewPriorityController;
-use App\Http\Controllers\RankingController;
-use App\Http\Controllers\SagaController;
-use App\Http\Controllers\StatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function(){
@@ -19,14 +11,5 @@ Route::controller(AuthController::class)->group(function(){
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('/pending-priorities', PendingPriorityController::class)->only(['index']);
-    Route::apiResource('/post-view-priorities', PostViewPriorityController::class)->only(['index']);
     Route::apiResource('/categories', CategoryController::class)->only(['index']);
-    Route::apiResource('/statuses', StatusController::class)->only(['index']);
-    Route::apiResource('/medias', MediumController::class);
-    Route::apiResource('/sagas', SagaController::class);
-    Route::apiResource('/chapters', ChapterController::class);
-    Route::apiResource('/favorites', FavoriteController::class);
-    Route::apiResource('/ranking', RankingController::class)->only(['index', 'show']);
-    Route::post('/add-image/{id}', [MediumController::class, 'addImage']);
 });
