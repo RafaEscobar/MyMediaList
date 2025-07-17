69061c0129f190f1a39cf46ca6a9b011e0291718
rafa <Rafael.EG.code@gmail.com>
2025-07-17 15:17:06 -0600

    Agrega filtro por categoria

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -11,7 +11,12 @@ class RankingController
     public function index(Request $request)
     {
         try {
-            $contents = Content::orderBy('score', 'desc')->limit(20)->get();
+            $contents = Content::when($request->has('category_id'), function($q) use ($request) {
+                $q->where('category_id', $request->input('category_id'));
+            })
+            ->orderBy('score', 'desc')
+            ->limit(20)
+            ->get();
             return new ContentCollection($contents, false);
         } catch (\Throwable $th) {
             return response()->json(["message" => $th->getMessage()], 500);
