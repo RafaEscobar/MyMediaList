e59250ecc60eac050eaf83a80df4b7e37649b6a9
rafa <Rafael.EG.code@gmail.com>
2025-07-17 16:21:07 -0600

    Ajuste previo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -16,6 +17,20 @@ class AuthController extends Controller
         try {
             if (Auth::attempt($request->all())) {
                 $user = User::findOrFail(Auth::user()->id);
+                $topCategory = $user->contents()
+                    ->select('category_id', DB::raw('COUNT(*) as total'))
+                    ->groupBy('category_id')
+                    ->orderByDesc('total')
+                    ->with('category')
+                    ->first();
+                $pendings = $user->contents()->where('status', 0)->count();
+                $favorites = $user->contents()->where('isFavorite', 1)->count();
+                $declined = $user->contents()->where('status', 2)->count();
+                $day = $user->contents()
+                    ->select(DB::raw('DAYNAME(created_at) as day'), DB::raw('COUNT(*) as total'))
+                    ->groupBy('day')
+                    ->orderByDesc('total')
+                    ->first();
                 $token = $user->createToken('auth_token')->plainTextToken;
                 return new UserResource($user, $token);
             } else {
